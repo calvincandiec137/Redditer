7854174a0979f9a92bddfcf72685c2a9d1087bf4
faiz <khanfaizmominshama@gmail.com>
2025-01-22 22:48:20 +0530

    clean slate

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", // Include this if you're using the app router
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
